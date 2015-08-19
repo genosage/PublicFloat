5295bec15ccbb6dfa2473c0b7187568cced75a77
winsomeking <winsomeking@gmail.com>
2015-08-19 20:44:59 +1000

    Created initial XCode project.

---
+++ iOS_client/PublicFloat/PublicFloat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PublicFloat
+//
+//  Created by Albert Wang on 19/08/2015.
+//  Copyright (c) 2015 YellowTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
