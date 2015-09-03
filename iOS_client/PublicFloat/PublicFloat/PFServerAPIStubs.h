70102f97cbe9db2072ccb470eae24aa0ad1cbb6a
winsomeking <winsomeking@gmail.com>
2015-09-03 22:54:22 +1000

    Added server api stubs.

---
+++ iOS_client/PublicFloat/PublicFloat/PFServerAPIStubs.h
@@ -0,0 +1,17 @@
+//
+//  PFServerAPIStubs.h
+//  PublicFloat
+//
+//  Created by Albert Wang on 3/09/2015.
+//  Copyright (c) 2015 YellowTeam. All rights reserved.
+//
+
+#if DEBUG
+
+#import <Foundation/Foundation.h>
+
+@interface PFServerAPIStubs : NSObject
+
+@end
+
+#endif
