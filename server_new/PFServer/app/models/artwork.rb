8ba5c913dd24f35e9d4e7851c759454490b491c5
winsomeking <winsomeking@gmail.com>
2015-09-20 21:04:26 +1000

    [5] added validation for artwork model. updated tests as well.

--- server_new/PFServer/app/models/artwork.rb
+++ server_new/PFServer/app/models/artwork.rb
@@ -1,2 +1,11 @@
 class Artwork < ActiveRecord::Base
+    validates :title, presence: true
+    validates :artwork_longtitude, presence: true, uniqueness: true
+    validates :artwork_latitude, presence: true, uniqueness: true
+    validates :description, presence: true, uniqueness: true
+    validates :image_url, allow_blank: true
+    # , format: { with: %r{\.(gif|jpg|png)\A}i, message: 'must be a URL for GIF, JPG or PNG image.'}
+    validates :image_url, uniqueness: true
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
--- server_new/PFServer/test/controllers/artworks_controller_test.rb
+++ server_new/PFServer/test/controllers/artworks_controller_test.rb
@@ -3,43 +3,51 @@ require 'test_helper'
 class ArtworksControllerTest < ActionController::TestCase
   setup do
     @artwork = artworks(:one)
+    @update = {
+        title: 'Lorem Ipsum',
+        description: 'A great piece of art,A great piece of art,A great piece of art,A great piece of art,A great piece of art',
+        image_url: 'image.jpg',
+        name: 'artwork name',
+        artwork_longtitude: 123456.1,
+        artwork_latitude: 123456.1
+    }
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:artworks)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create artwork" do
+  test 'should create artwork' do
     assert_difference('Artwork.count') do
-      post :create, artwork: { artwork_latitude: @artwork.artwork_latitude, artwork_longtitude: @artwork.artwork_longtitude, description: @artwork.description, image_url: @artwork.image_url, name: @artwork.name, title: @artwork.title }
+      post :create, artwork: @update  #{ artwork_latitude: @artwork.artwork_latitude, artwork_longtitude: @artwork.artwork_longtitude, description: @artwork.description, image_url: @artwork.image_url, name: @artwork.name, title: @artwork.title }
     end
 
     assert_redirected_to artwork_path(assigns(:artwork))
   end
 
-  test "should show artwork" do
+  test 'should show artwork' do
     get :show, id: @artwork
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @artwork
     assert_response :success
   end
 
-  test "should update artwork" do
-    patch :update, id: @artwork, artwork: { artwork_latitude: @artwork.artwork_latitude, artwork_longtitude: @artwork.artwork_longtitude, description: @artwork.description, image_url: @artwork.image_url, name: @artwork.name, title: @artwork.title }
+  test 'should update artwork' do
+    put :update, id: @artwork, artwork: @update
     assert_redirected_to artwork_path(assigns(:artwork))
   end
 
-  test "should destroy artwork" do
+  test 'should destroy artwork' do
     assert_difference('Artwork.count', -1) do
       delete :destroy, id: @artwork
     end
