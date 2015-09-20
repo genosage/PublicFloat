a889acdfe50c2f2ec26deb10efc607cb6983c82b
winsomeking <winsomeking@gmail.com>
2015-09-20 16:21:07 +1000

    [5] created artworks table, model, view and controller.

---
+++ server_new/PFServer/app/views/artworks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @artwork, :id, :name, :description, :artwork_longtitude, :artwork_latitude, :title, :image_url, :created_at, :updated_at
---
+++ server_new/PFServer/test/controllers/artworks_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class ArtworksControllerTest < ActionController::TestCase
+  setup do
+    @artwork = artworks(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:artworks)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create artwork" do
+    assert_difference('Artwork.count') do
+      post :create, artwork: { artwork_latitude: @artwork.artwork_latitude, artwork_longtitude: @artwork.artwork_longtitude, description: @artwork.description, image_url: @artwork.image_url, name: @artwork.name, title: @artwork.title }
+    end
+
+    assert_redirected_to artwork_path(assigns(:artwork))
+  end
+
+  test "should show artwork" do
+    get :show, id: @artwork
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @artwork
+    assert_response :success
+  end
+
+  test "should update artwork" do
+    patch :update, id: @artwork, artwork: { artwork_latitude: @artwork.artwork_latitude, artwork_longtitude: @artwork.artwork_longtitude, description: @artwork.description, image_url: @artwork.image_url, name: @artwork.name, title: @artwork.title }
+    assert_redirected_to artwork_path(assigns(:artwork))
+  end
+
+  test "should destroy artwork" do
+    assert_difference('Artwork.count', -1) do
+      delete :destroy, id: @artwork
+    end
+
+    assert_redirected_to artworks_path
+  end
+end
